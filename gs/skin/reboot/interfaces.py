867b614393e9defea76c0f73932b236e0af0c23f
Mistral Oz <mistral@ozm.fr>
2016-02-07 20:00:21 +0100

    -

---
+++ gs/skin/reboot/interfaces.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from zope.app.rotterdam import Rotterdam
+from zope.publisher.interfaces.browser import IBrowserRequest
+
+
+class IGSBlueLayer(IBrowserRequest):
+    '''GroupServer Blue Layer'''
+    # Layers just inherit from IBrowserRequest.
+
+
+class IGSBlueSkin(IGSBlueLayer, Rotterdam):
+    '''GroupServer Blue Skin'''
+    # Skins inherit from a bunch of layers. In this case the blue layer
+    # and the core Zope Rotterdam layer.
